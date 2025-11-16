c0f15700d3d7a68f76277b1ec62624f7d694537b
Evan Woods <woodsevanc@gmail.com>
2025-11-16 18:11:29 +1100

    Selected all top of screen AI results / views and set their display to none

--- content.js
+++ content.js
@@ -1,7 +1,14 @@
-// const main = () => {
-//     console.log("Remove AI Summary extension loaded.");
-// }
+// AI Overivew class names: OZ9ddf WAUd4
+const main = () => {
+    console.log("AI Summary extension loaded.");
+    console.log("Remove AI Summary extension loaded.");
+    // AI Overview title
+    document.querySelector(".OZ9ddf").setAttribute("style", "display: none !important;");
+    // AI Overview content
+    document.getElementById("Odp5De").setAttribute("style", "display: none !important;");
+    // AI Mode Button (in header next to all, videos, images options)
+    document.querySelector(".R1QWuf").setAttribute("style", "display: none !important;");
+    console.log(overviewSelector);
+}
 
-// main();
-
-console.log("REMOVE AI SUMMARY EXTENSION LOADED");
+main();
